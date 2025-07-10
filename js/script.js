6551858f29f9384dbf85065c13b6225693afbd7a
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-10 17:27:28 +0200

    milestone 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-const km = document.getElementById(`km`);
+const kmInput = document.getElementById(`km`);
+const ageInput = document.getElementById(`age`);
+const button = document.getElementById(`button`);
+
+
+button.addEventListener(`click`, function (event) {
+  event.preventDefault();
+  const km = parseInt(kmInput.value);
+  const age = parseInt(ageInput.value);
+  const basePrice = km * 0.21
+  let finalPrice;
+
+  console.log(km, age, basePrice)
+
+  if (age < 18) {
+    finalPrice = basePrice - basePrice * 20 / 100
+  }
+  else if (age > 65) {
+    finalPrice = basePrice - basePrice * 40 / 100
+  }
+
+  else {
+    finalPrice = basePrice
+  }
+
+  console.log(`${finalPrice.toFixed(2)}`)
+})
